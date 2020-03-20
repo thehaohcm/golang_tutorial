0069d09c507f50741eef41d4fc843fa9c5ea2764
Nguyen The Hao <hao.nguyen@s3corp.com.vn>
2020-03-20 19:06:03 +0700

    update jwt

--- Assigment/main.go
+++ Assigment/main.go
@@ -6,6 +6,20 @@ import (
 	"net/http"
 )
 
+// @title Golang API endpoints assignment
+// @version 1.0
+// @description This is a sample of Golang API endpoints assignment
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name Hao Nguyen
+// @contact.url http://musicmaven.s3corp.vn
+// @contact.email hao.nguyen@s3corp.com.vn
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath /
 func main() {
 	fmt.Println("Server started on: http://localhost:8080")
 	http.ListenAndServe(":8080", routes.CreateRouter())
--- Assigment/routes/routes.go
+++ Assigment/routes/routes.go
@@ -4,27 +4,62 @@ import (
 	friend "Assigment/controllers/friend"
 	subscribe "Assigment/controllers/subscribe"
 	user "Assigment/controllers/user"
+	"fmt"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/jwtauth"
 	httpSwagger "github.com/swaggo/http-swagger"
 
 	_ "Assigment/docs"
 )
 
+var tokenAuth *jwtauth.JWTAuth
+
+func init() {
+	tokenAuth = jwtauth.New("HS256", []byte("secret"), nil)
+
+	// For debugging/example purposes, we generate and print
+	// a sample jwt token with claims `user_id:123` here:
+	_, tokenString, _ := tokenAuth.Encode(jwt.MapClaims{"user_id": 123})
+	fmt.Printf("DEBUG: a sample jwt is %s\n\n", tokenString)
+}
+
 func CreateRouter() *chi.Mux {
 	router := chi.NewRouter()
 
-	router.Get("/showAllUsers", user.ShowAllUser)
-	router.Post("/addFriend", friend.AddFriend)
-	router.Get("/listFriend", friend.ShowListFriend)
-	router.Post("/commonFriend", friend.ShowCommonFriends)
-	router.Post("/subscribe", subscribe.Subscribe)
-	router.Post("/blockSubscribe", subscribe.BlockSubscribe)
-	router.Post("/listRecipients", subscribe.ShowListRecipients)
+	router.Group(func(r chi.Router) {
+		r.Use(jwtauth.Verifier(tokenAuth))
 
-	router.Get("/swagger/*", httpSwagger.Handler(
-		httpSwagger.URL("http://localhost:8080/swagger/doc.json"), //The url pointing to API definition"
-	))
+		r.Use(jwtauth.Authenticator)
+
+		r.Post("/addFriend", friend.AddFriend)
+		r.Get("/showAllUsers", user.ShowAllUser)
+	})
+
+	router.Group(func(r chi.Router) {
+		// router.Post("/addFriend", friend.AddFriend)
+		r.Get("/listFriend", friend.ShowListFriend)
+		r.Post("/commonFriend", friend.ShowCommonFriends)
+		r.Post("/subscribe", subscribe.Subscribe)
+		r.Post("/blockSubscribe", subscribe.BlockSubscribe)
+		r.Post("/listRecipients", subscribe.ShowListRecipients)
+
+		r.Get("/swagger/*", httpSwagger.Handler(
+			httpSwagger.URL("http://localhost:8080/swagger/doc.json"), //The url pointing to API definition"
+		))
+	})
+	// router.Get("/showAllUsers", user.ShowAllUser)
+	// // router.Post("/addFriend", friend.AddFriend)
+	// router.Get("/listFriend", friend.ShowListFriend)
+	// router.Post("/commonFriend", friend.ShowCommonFriends)
+	// router.Post("/subscribe", subscribe.Subscribe)
+	// router.Post("/blockSubscribe", subscribe.BlockSubscribe)
+	// router.Post("/listRecipients", subscribe.ShowListRecipients)
+
+	// router.Get("/swagger/*", httpSwagger.Handler(
+	// 	httpSwagger.URL("http://localhost:8080/swagger/doc.json"), //The url pointing to API definition"
+	// ))
 
 	return router
 }
